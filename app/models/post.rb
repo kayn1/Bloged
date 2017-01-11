344b2d198470f9a71860abba3fa697213e086336
highassfak <kayn1@wp.pl>
2017-01-11 21:52:41 +0100

    Second

---
+++ app/models/post.rb
@@ -0,0 +1,16 @@
+class Post < ApplicationRecord
+  extend FriendlyId
+  friendly_id :title, use: :slugged
+
+  belongs_to :author
+
+  scope :most_recent, -> { order(id: :desc) }
+
+  def should_generate_new_friendly_id?
+  	title_changed?
+  end
+
+  def published_date
+  	created_at.strftime('%-d - %B - %Y')
+  end
+end
