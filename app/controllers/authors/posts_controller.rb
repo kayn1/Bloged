0f96c54236a8ce062204538fadecd29671cfcb5c
highassfak <kayn1@wp.pl>
2017-02-05 13:20:19 +0100

    lel

--- app/controllers/authors/posts_controller.rb
+++ app/controllers/authors/posts_controller.rb
@@ -7,7 +7,7 @@ module Authors
   def index
     @posts = current_author.posts.most_recent
     @posts.order(published_at: :desc)
-    @posts = Post.published
+    @@posts = Post.published
 
   end
 
