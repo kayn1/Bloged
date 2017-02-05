dbe2c10cc34181dafbea6dde0c66a47cb8ff6c12
highassfak <kayn1@wp.pl>
2017-02-05 21:10:04 +0100

    afa

--- app/controllers/authors/posts_controller.rb
+++ app/controllers/authors/posts_controller.rb
@@ -6,7 +6,7 @@ module Authors
   # GET /posts.json
   def index
     @posts = current_author.posts.most_recent
-    @posts.order(id: :desc)
+    @posts.order(id: :asc)
     @@posts = Post.published
 
   end
